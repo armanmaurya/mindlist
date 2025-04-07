d946df5c51b852dc4b51afbe17338db5cf2e73f9
Arman Maurya <armanmarya6@gmail.com>
2025-04-07 20:05:47 +0530

    Optimized Code Performance and refactored code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,7 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:implicitly_animated_reorderable_list_2/transitions.dart';
 import 'package:todo_native/controllers/todo_controller.dart';
 import 'package:todo_native/widgets/add_todo.dart';
-import 'package:todo_native/widgets/todo_item.dart';
-import '../models/todo.dart';
-import 'package:implicitly_animated_reorderable_list_2/implicitly_animated_reorderable_list_2.dart';
+import 'package:todo_native/widgets/todo_list_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,54 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Todo List')),
-      body: ValueListenableBuilder<List<Todo>>(
-        valueListenable: _todoController.todos,
-        builder:(context, todos, _) {
-          if (todos.isEmpty) {
-            return const Center(child: Text("List is Empty"));
-          }
-
-          return ImplicitlyAnimatedReorderableList<Todo>(
-            items: todos,
-            areItemsTheSame: (oldItem, newItem) => oldItem.title == newItem.title,
-            onReorderFinished: (item, from, to, newItems) {
-              _todoController.saveReorderedTodos(newItems);
-            },
-            itemBuilder: (context, itemAnimation, todo, index) {
-              return Reorderable(
-                key: ValueKey(todo.title),
-                builder: (context, dragAnimation, inDrag) {
-                  final t = dragAnimation.value;
-                  final elevation = lerpDouble(0, 8, t);
-                  final color = Color.lerp(Colors.white, Colors.white.withOpacity(0.8), t);
-                  return SizeFadeTransition(
-                    sizeFraction: 1,
-                    curve: Curves.easeInOut,
-                    animation: itemAnimation,
-                    child: AnimatedScale(
-                      scale: inDrag ? 1.1 : 1.0,
-                      duration: const Duration(milliseconds: 200),
-                      curve: Curves.easeInOut,
-                      child: Material(
-                        color: color,
-                        elevation: elevation!,
-                        type: MaterialType.transparency,
-                        child: TodoItem(
-                          onDelete: () => _todoController.deleteTodo(index),
-                          onToggle: (_) => _todoController.toggleTodo(index),
-                          onUpdate: (newTitle) => _todoController.updateTodo(newTitle, index),
-                          todo: todo,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          );
-        }
-
-      ),
+      body: TodoListView(controller: _todoController),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await showDialog(
---
+++ lib/widgets/todo_list_view.dart
@@ -0,0 +1,84 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:implicitly_animated_reorderable_list_2/implicitly_animated_reorderable_list_2.dart';
+import 'package:implicitly_animated_reorderable_list_2/transitions.dart';
+import 'package:todo_native/controllers/todo_controller.dart';
+import 'package:todo_native/models/todo.dart';
+import 'package:todo_native/widgets/todo_item.dart';
+
+// class TodoListView extends StatefulWidget {
+//   const TodoListView({super.key});
+
+//   @override
+//   State<TodoListView> createState() => _TodoListViewState();
+// }
+
+class TodoListView extends StatelessWidget{
+  final TodoController controller;
+
+  const TodoListView({
+    super.key,
+    required this.controller
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<List<Todo>>(
+      valueListenable: controller.todos,
+      builder: (context, todos, _) {
+        if (todos.isEmpty) {
+          return const Center(child: Text("List is Empty"));
+        }
+
+        return _buildListView(todos);
+      },
+    );
+  }
+
+  Widget _buildListView(List<Todo> todos) {
+    return ImplicitlyAnimatedReorderableList<Todo>(
+      items: todos,
+      areItemsTheSame: (oldItem, newItem) => oldItem.title == newItem.title,
+      onReorderFinished: (item, from, to, newItems) {
+        controller.saveReorderedTodos(newItems);
+      },
+      itemBuilder: (context, itemAnimation, todo, index) {
+        return Reorderable(
+          key: ValueKey(todo.title),
+          builder: (context, dragAnimation, inDrag) {
+            final t = dragAnimation.value;
+            final elevation = lerpDouble(0, 8, t);
+            final color = Color.lerp(
+              Colors.white,
+              Colors.white.withValues(alpha: 0.8),
+              t,
+            );
+            return SizeFadeTransition(
+              sizeFraction: 1,
+              curve: Curves.easeInOut,
+              animation: itemAnimation,
+              child: AnimatedScale(
+                scale: inDrag ? 1.1 : 1.0,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeInOut,
+                child: Material(
+                  color: color,
+                  elevation: elevation!,
+                  type: MaterialType.transparency,
+                  child: TodoItem(
+                    onDelete: () => controller.deleteTodo(index),
+                    onToggle: (_) => controller.toggleTodo(index),
+                    onUpdate:
+                        (newTitle) => controller.updateTodo(newTitle, index),
+                    todo: todo,
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
